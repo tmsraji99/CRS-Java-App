f745beaa73c93d0e220313cf165203ea4d5ea5ad
DESKTOP-6OUJO4M\Admin <tmsraji99@gmail.com>
2021-11-18 11:44:07 +0530

    Exception class added

---
+++ CRS/src/com/lti/exception/MaxCoursesOpted.java
@@ -0,0 +1,10 @@
+package com.lti.exception;
+
+public class MaxCoursesOpted extends Exception {
+
+	public String getMessage() {
+		String msg="Max courses Opted Please check again ";
+		return msg;
+	}
+	
+}
